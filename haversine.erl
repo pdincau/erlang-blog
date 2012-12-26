95c5ba8c72b6475ca4c25883744f75a5fb57e5a0
Paolo <paolo.dincau@gmail.com>
2012-12-26 13:40:41 +0100

    add haversine.erl

---
+++ haversine.erl
@@ -0,0 +1,16 @@
+-module(haversine).
+-export([distance/4]).
+
+distance(Lng1, Lat1, Lng2, Lat2) ->
+    ToRadians = fun(Deg) -> 2*math:pi()*Deg/360 end,
+    [RLng1, RLat1, RLng2, RLat2] = [ToRadians(Deg) || Deg <- [Lng1, Lat1, Lng2, Lat2]],
+
+    DLon = RLng2 - RLng1,
+    DLat = RLat2 - RLat1,
+
+    A = math:pow(math:sin(DLat/2), 2) + math:cos(Lat1) * math:cos(Lat2) * math:pow(math:sin(DLon/2), 2),
+
+    C = 2 * math:asin(math:sqrt(A)),
+    
+    Km = 6371 * C,
+    Km.
