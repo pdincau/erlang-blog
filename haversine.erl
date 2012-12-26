0b78e255b2136dc5ebd4a27ca61231a9d08aeb47
Paolo <paolo.dincau@gmail.com>
2012-12-26 14:56:45 +0100

    fix error in haversine function

--- haversine.erl
+++ haversine.erl
@@ -2,16 +2,16 @@
 -export([distance/4]).
 
 distance(Lng1, Lat1, Lng2, Lat2) ->
-    ToRadians = fun(Deg) -> 2*math:pi()*Deg/360 end,
-    [RLng1, RLat1, RLng2, RLat2] = [ToRadians(Deg) || Deg <- [Lng1, Lat1, Lng2, Lat2]],
+    Deg2rad = fun(Deg) -> math:pi()*Deg/180 end,
+    [RLng1, RLat1, RLng2, RLat2] = [Deg2rad(Deg) || Deg <- [Lng1, Lat1, Lng2, Lat2]],
 
     DLon = RLng2 - RLng1,
     DLat = RLat2 - RLat1,
 
-    A = math:pow(math:sin(DLat/2), 2) + math:cos(Lat1) * math:cos(Lat2) * math:pow(math:sin(DLon/2), 2),
+    A = math:pow(math:sin(DLat/2), 2) + math:cos(RLat1) * math:cos(RLat2) * math:pow(math:sin(DLon/2), 2),
 
     C = 2 * math:asin(math:sqrt(A)),
     
-    %% consider Earth radius equal to 6371km
-    Km = 6371 * C,
+    %% suppose radius of Earth is 6372.8 km
+    Km = 6372.8 * C,
     Km.
