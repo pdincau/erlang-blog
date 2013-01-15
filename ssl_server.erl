5d126c82360af3fb8782db5bbe3f4f34aae6eee5
Paolo <paolo.dincau@gmail.com>
2013-01-15 13:07:41 +0100

    commit ssl server stub

---
+++ ssl_server.erl
@@ -0,0 +1,145 @@
+%%%-------------------------------------------------------------------
+%%% @author Paolo D'Incau <paolo.dincau@gmail.com>
+%%% @copyright (C) 2013, Paolo D'Incau
+%%% @doc
+%%%
+%%% @end
+%%% Created : 12 Jan 2013 by Paolo D'Incau <paolo.dincau@gmail.com>
+%%%-------------------------------------------------------------------
+-module(ssl_server).
+
+-behaviour(gen_server).
+
+%% API
+-export([start_link/1]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+	 terminate/2, code_change/3]).
+
+-define(SERVER, ?MODULE). 
+
+-record(state, {socket}).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server
+%%
+%% @spec start_link(LSocket) -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link(LSocket) ->
+    gen_server:start_link(?MODULE, [LSocket], []).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init([LSocket]) ->
+    gen_server:cast(self(), accept),
+    {ok, #state{socket = LSocket}}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @spec handle_call(Request, From, State) ->
+%%                                   {reply, Reply, State} |
+%%                                   {reply, Reply, State, Timeout} |
+%%                                   {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, Reply, State} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_call(_Request, _From, State) ->
+    Reply = ok,
+    {reply, Reply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @spec handle_cast(Msg, State) -> {noreply, State} |
+%%                                  {noreply, State, Timeout} |
+%%                                  {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_cast(accept, #state{socket = LSocket} = State) ->
+    ssl:setopts(LSocket, [{active, false}]),
+    {ok, NewSocket} = ssl:transport_accept(LSocket),
+    ok = ssl:ssl_accept(NewSocket),
+    ssl_server_sup:start_socket(),
+    io:format("Connection opened.~n", []),
+    {noreply, State#state{socket = NewSocket}};
+
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_info({ssl_closed, _Socket}, State) ->
+    io:format("Connection closed by peer.~n", []),
+    {stop, normal, State};
+
+handle_info({ssl, _Socket, Data}, State) ->
+    io:format("Received: ~p~n", [Data]),
+    {noreply, State};
+
+handle_info(_Info, State) ->
+    io:format("Received unknown message: ~p~n", [_Info]),
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
